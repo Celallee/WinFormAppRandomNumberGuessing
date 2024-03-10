6142784a961fca0e4c338c09062d6fb31565e86b
Celal Yıldırım <celallee12@gmail.com>
2024-03-10 18:25:02 +0300

    - Feature: Random number finding method and control feature added

--- Form1.cs
+++ Form1.cs
@@ -16,22 +16,86 @@ namespace WinFormAppRandomNumberGuessing
         {
             InitializeComponent();
         }
-        
+        int rastgele, sayac;
         private void btn_start_Click(object sender, EventArgs e)
         {
 
+            // MessageBox.Show("Start");	
+            lbl_durum.Visible = true;
+            txt_sayi.Text = "";
+            btn_control.Enabled = true;
+            Random random = new Random();
+            rastgele = random.Next(0, 100);
+            sayac = 10;
+            lbl_sayac.Text = sayac.ToString() + " Hakkınız var";
+            lbl_durum.Text = rastgele.ToString();
+            lbl_durum.Text = "";
+            lbl_sayac.Text = "";
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            lbl_durum.Visible = false;
+            btn_control.Enabled = false;
+            lbl_sayac.Text = "10 Hakkınız var";
 
         }
 
         private void btn_control_Click(object sender, EventArgs e)
         {
-            
+            // MessageBox.Show("Kontrol");
+            if (txt_sayi.Text == "")
+            {
+                lbl_durum.Visible = true;
+                lbl_durum.Text = "Sayi giriniz";
+                sayac--;
+            }
+            else
+            {
+                int girilensayi = Convert.ToInt32(txt_sayi.Text);
+                if (girilensayi < rastgele)
+                {
+                    lbl_durum.Text = "Arttır";
+                    sayac--;
+                    lbl_sayac.Text = sayac.ToString() + " Hakkınız kaldı";
+                }
+                else if (girilensayi > rastgele)
+                {
+                    lbl_durum.Text = "Azalt";
+                    sayac--;
+                    lbl_sayac.Text = sayac.ToString() + " Hakkınız kaldı";
+                }
+                else
+                {
+                    lbl_durum.Text = "Kazandınız";
+                    btn_control.Enabled = false;
+                }
+            }
+            if (sayac == 0)
+            {
+                lbl_durum.Text = "Kazanamadın";
+                btn_control.Enabled = false;
+
+
+
+                if (MessageBox.Show("Kaybettiniz ", "Tekrar oynamak İstermisiniz", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                return;
+
+            }
+
+
         }
     }
 
